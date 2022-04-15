caa002034c83ad4a3110dca6e820a59643caee94
Carolyne Oliver <66889140+carolyneoliverr@users.noreply.github.com>
2022-04-15 18:46:44 -0300

    Update resolucao.js

--- exercicio-38/resolucao.js
+++ exercicio-38/resolucao.js
@@ -36,7 +36,13 @@ function ehPalindromo(palavra) {
  * `INFELIZMENTE, NÃO SOU UM PALÍNDROMO`
  */
 // escreva aqui seu código:
-
+function checkPalindrome(str) {     
+    for(var i = 0; i < str.length / 2; i++) if (str[i] != str[str.length - i - 1]) return false;
+    return true;
+}
+console.log(checkPalindrome("radar"));
+console.log(checkPalindrome("reviver"));
+console.log(checkPalindrome("palindromo"));
     
  
 
